56b0aaf00e7636b8efbff9a9341023a98f35b4f1
ZC502 <liouzuocen@qq.com>
2025-10-03 15:38:48 +0800

    Update hackflight.cpp

--- webots/controllers/hackflight/hackflight.cpp
+++ webots/controllers/hackflight/hackflight.cpp
@@ -1,1 +1,19 @@
+#include <webots/Robot.hpp>
+#include <webots/InertialUnit.hpp>
+#include "../src/hackflight.h"  // Your aggregated header
 
+int main(int argc, char **argv) {
+    webots::Robot *robot = new webots::Robot();
+    webots::InertialUnit *imu = robot->getDevice<webots::InertialUnit>("imu");
+
+    // Init Hackflight
+    Hackflight hf;
+    hf.init();  // Your init with noise_params
+
+    while (robot->step(10) != -1) {  // 100Hz
+        hf.update();  // Run loop with noisy sensors
+    }
+
+    delete robot;
+    return 0;
+}
